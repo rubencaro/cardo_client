52f081248e8ea75800142548870b95275a3335b8
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-04-14 17:08:34 +0200

    First real dispatcher

--- app/store/socket/dispatcher.js
+++ app/store/socket/dispatcher.js
@@ -1,8 +1,29 @@
 // The dispatching code of the connector
 
+const routes = calcRoutes()
+
 // This is called for every message coming into the socket
 function dispatch(store, e) {
-  store.dispatch("addLogLine", e.data)
+  let [prefix, payload] = splitMsg(e.data)
+
+  let action = routes.get(prefix)
+  if (action === undefined) {
+    console.log("Unrecognized msg: " + e.data)
+    return
+  }
+  store.dispatch(action, payload)
+}
+
+function calcRoutes() {
+  let kvs = [
+    // here add [route, action] pairs
+    ['log', 'addLogLine'],
+  ];
+  return new Map(kvs)
+}
+
+function splitMsg(msg) {
+  return msg.split(": ", 2)
 }
 
 export { dispatch }
